12c43de6bb253da44799b518aa85297e78cdb37f
Liliya <liliyavoloshine@gmail.com>
2021-12-09 20:20:40 +0400

    init: start portfolio project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Header />
+</template>
+
+<script>
+import Header from './components/Header.vue'
+export default {
+  name: 'App',
+  components: { Header }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>header</header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {}
+}
+</script>
